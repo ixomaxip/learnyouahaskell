7afd8dd3a9c27cefc99c32cf2f5250ac7f4f27ee
Max <max.white.rabbit@gmail.com>
2019-03-31 18:38:22 +0500

    =

--- ch1/baby.hs
+++ ch1/baby.hs
@@ -5,6 +5,7 @@ doubleUs x y = doubleMe x + doubleMe y
 doubleSmallNumber x = if x > 100
                         then x
                         else x*2
+
 bB xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
 
 rightTriangles x = [(a,b,c) | c <- [1..x], a <- [1..c], b <- [1..a], a^2 + b^2 == c^2]

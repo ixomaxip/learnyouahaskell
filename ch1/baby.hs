c5cffa383b9d0958ef74e7222aca740e0017ed90
Max <max.white.rabbit@gmail.com>
2019-04-01 00:12:07 +0500

    add signatures to functions in baby

--- ch1/baby.hs
+++ ch1/baby.hs
@@ -1,11 +1,17 @@
+doubleMe :: Num a => a -> a
 doubleMe x = x + x
 
+doubleUs :: Num a => a -> a -> a
 doubleUs x y = doubleMe x + doubleMe y
 
+doubleSmallNumber :: (Num a, Ord a) =>  a -> a
 doubleSmallNumber x = if x > 100
                         then x
                         else x*2
 
+bB :: Integral a => [a] -> [[Char]]
 bB xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
 
+
+rightTriangles :: (Enum t, Eq t, Num t) => t -> t -> [(t,t,t)]
 rightTriangles x y = [(a,b,c) | c <- [1..x], a <- [1..c], b <- [1..a], a^2 + b^2 == c^2, a + b + c == y]

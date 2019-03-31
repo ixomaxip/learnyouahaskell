e076659410a28a57592718c6c1c6c369079e8a04
Max <max.white.rabbit@gmail.com>
2019-04-01 00:16:12 +0500

    simple factorial

--- ch2/fs.hs
+++ ch2/fs.hs
@@ -3,3 +3,6 @@ removeNonUppercase st = [ c | c <- st, elem c ['A'..'Z']]
 
 addThree :: Int -> Int -> Int -> Int
 addThree x y z = x + y + z
+
+factorial :: Integer -> Integer
+factorial n = product [1..n]

9f81eec42abfb12f1aeecf75dc4ff57bd385396b
Max <max.white.rabbit@gmail.com>
2019-03-31 17:37:21 +0500

    list of comprehension: BOOM BANG function

--- ch1/baby.hs
+++ ch1/baby.hs
@@ -5,3 +5,4 @@ doubleUs x y = doubleMe x + doubleMe y
 doubleSmallNumber x = if x > 100
                         then x
                         else x*2
+bB xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]

38984465762f3b9e83f8ec0639492fca51194ae6
Max <max.white.rabbit@gmail.com>
2019-04-06 14:02:29 +0500

    recursive factorial

--- ch3/fn.hs
+++ ch3/fn.hs
@@ -6,4 +6,8 @@ sayMe :: Int -> String
 sayMe 1 = "One"
 sayMe 2 = "Two"
 sayMe 3 = "Three"
-sayMe x = "Toooooooo much" 
+sayMe x = "Toooooooo much"
+
+factorial :: Int -> Int
+factorial 0 = 1
+factorial n = n * factorial (n - 1)

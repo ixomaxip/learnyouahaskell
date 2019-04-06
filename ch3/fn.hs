ad25ac6baaf0ed1e35ff8008af61af06727bb810
Max <max.white.rabbit@gmail.com>
2019-04-06 13:57:54 +0500

    lucky &  say me

---
+++ ch3/fn.hs
@@ -0,0 +1,9 @@
+lucky :: Int -> String
+lucky 7 = "LUCKY NUMBER!"
+lucky x = "YOU ARE DEAD"
+
+sayMe :: Int -> String
+sayMe 1 = "One"
+sayMe 2 = "Two"
+sayMe 3 = "Three"
+sayMe x = "Toooooooo much" 
